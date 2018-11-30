a300754ef22965569290aca7c2b09ec6cd39cb05
Jan Freyberg <jan.freyberg@gmail.com>
2018-11-30 16:15:34 +0000

    Use distribution version number in package

--- faculty_mill/version.py
+++ faculty_mill/version.py
@@ -1,4 +1,12 @@
-__version__ = "0.0.1"
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+
 version = __version__
 
 
